b4e4baa6938d68f37d6ce84e445144cc902a6fed
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-03-23 18:29:21 +0100

    yo

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -6,12 +6,12 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 16:45:39 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/03/23 17:38:07 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/03/23 18:25:38 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-int i = 0;
+
 int	main(int argc, char **argv)
 {
 	t_param	param;
@@ -20,7 +20,6 @@ int	main(int argc, char **argv)
 	fill_param(&param, argc, argv);
 	sem_t *yo = sem_open("yo", O_CREAT, 0660, param.number_of_philo);
 	sem_t *ready = sem_open("ready", O_CREAT, 0660, 1);
-	uint64_t	t_zero = get_time();
 	sem_wait(ready);
 	for (int i = 0; i < param.number_of_philo; i++)
 	{
@@ -28,6 +27,7 @@ int	main(int argc, char **argv)
 		{
 			sem_wait(ready);
 			sem_post(ready);
+			uint64_t	t_zero = get_time();
 			while (true)
 			{
 				sem_wait(yo);
