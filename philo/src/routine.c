07086f58166a64be2d7e41b11bab0f1a92524022
valentinRog <valentin.rogister@gmail.com>
2022-09-04 20:46:18 +0200

    fixed potential deadlock

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 17:18:53 by root              #+#    #+#             */
-/*   Updated: 2022/09/04 18:48:15 by root             ###   ########.fr       */
+/*   Updated: 2022/09/04 20:42:49 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,46 +20,55 @@ static void	monitor(t_philo *p, t_game *g, char *action)
 	pthread_mutex_unlock(&g->print_lock);
 }
 
-static void	pick_forks(t_philo *p, t_game *g)
+static bool	pick_forks(t_philo *p, t_game *g)
 {
 	if (g->n % 2 && p->i == g->n - 1)
+		while (g->turn != 2 && g->running)
+			;
+	else
+		while (g->turn != p->i % 2 && g->running)
+			;
+	if (!g->running)
+		return (true);
+	if (p->i < (p->i + 1) % g->n)
 	{
-		while (g->turn != 2)
-			if (!g->running)
-				return ;
+		pthread_mutex_lock(g->forks + p->i);
+		monitor(p, g, FORK);
+		pthread_mutex_lock(g->forks + (p->i + 1) % g->n);
+		monitor(p, g, FORK);
 	}
 	else
 	{
-		while (g->turn != p->i % 2)
-			if (!g->running)
-				return ;
+		pthread_mutex_lock(g->forks + (p->i + 1) % g->n);
+		monitor(p, g, FORK);
+		pthread_mutex_lock(g->forks + p->i);
+		monitor(p, g, FORK);
 	}
-	pthread_mutex_lock(g->forks + p->i);
-	monitor(p, g, FORK);
-	pthread_mutex_lock(g->forks + (p->i + 1) % g->n);
-	monitor(p, g, FORK);
-	g->ready_to_eat++;
-	if (g->ready_to_eat == g->n / 2 || g->turn == 2)
-	{
-		g->ready_to_eat = 0;
-		g->turn++;
-		g->turn %= 3;
-		if (g->turn == 2 && !(g->n % 2))
-			g->turn = 0;
-	}
+	return (false);
 }
 
 static void	eat(t_philo *p, t_game *g)
 {
-	pick_forks(p, g);
-	monitor(p, g, EAT);
-	milli_sleep(g->time_to_eat);
-	pthread_mutex_unlock(g->forks + p->i);
-	pthread_mutex_unlock(g->forks + (p->i + 1) % g->n);
-	p->last_eat = get_time();
-	p->n_eaten++;
-	if (p->n_eaten == g->n_eat)
-		g->ate_enough++;
+	if (!pick_forks(p, g))
+	{
+		g->ready_to_eat++;
+		if (g->ready_to_eat == g->n / 2 || g->turn == 2)
+		{
+			g->ready_to_eat = 0;
+			g->turn++;
+			g->turn %= 3;
+			if (g->turn == 2 && !(g->n % 2))
+				g->turn = 0;
+		}
+		monitor(p, g, EAT);
+		milli_sleep(g->time_to_eat);
+		pthread_mutex_unlock(g->forks + p->i);
+		pthread_mutex_unlock(g->forks + (p->i + 1) % g->n);
+		p->last_eat = get_time();
+		p->n_eaten++;
+		if (p->n_eaten == g->n_eat)
+			g->ate_enough++;
+	}
 }
 
 void	*philoop(void *arg)
